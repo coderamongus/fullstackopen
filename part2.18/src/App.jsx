a4abff5dff50789bd0bfd32a37cbb664d0b3aad4
Simo Ahlstrand <simo.ahlstrand@esedulainen.fi>
2024-09-17 13:28:18 +0300

    2.19

--- part2.18/src/App.jsx
+++ part2.18/src/App.jsx
@@ -4,67 +4,58 @@ import axios from 'axios';
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [filter, setFilter] = useState('');
-  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {
-    axios
-      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
-      .then((response) => {
-        setCountries(response.data);
-      })
-      .catch((error) => console.error('Error fetching country data:', error));
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all').then(response => {
+      setCountries(response.data);
+    });
   }, []);
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
+  const handleFilterChange = (event) => setFilter(event.target.value);
 
-  useEffect(() => {
-    const results = countries.filter(country =>
-      country.name.common.toLowerCase().includes(filter.toLowerCase())
-    );
-    setFilteredCountries(results);
-  }, [filter, countries]);
+  const handleShowCountry = (country) => setSelectedCountry(country);
 
-  const renderCountries = () => {
-    if (filteredCountries.length > 10) {
-      return <p>Too many matches, specify another filter.</p>;
-    } else if (filteredCountries.length > 1 && filteredCountries.length <= 10) {
-      return (
-        <ul>
-          {filteredCountries.map((country) => (
-            <li key={country.cca3}>{country.name.common}</li>
-          ))}
-        </ul>
-      );
-    } else if (filteredCountries.length === 1) {
-      const country = filteredCountries[0];
-      return (
-        <div>
-          <h2>{country.name.common}</h2>
-          <p>Capital: {country.capital[0]}</p>
-          <p>Population: {country.population}</p>
-          <h3>Languages:</h3>
-          <ul>
-            {Object.values(country.languages).map((language) => (
-              <li key={language}>{language}</li>
-            ))}
-          </ul>
-          <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="150" />
-        </div>
-      );
-    } else {
-      return <p>No matches found.</p>;
-    }
-  };
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Country Information</h1>
-      <div>
-        Find countries: <input value={filter} onChange={handleFilterChange} />
-      </div>
-      {renderCountries()}
+      <input value={filter} onChange={handleFilterChange} placeholder="Search for a country" />
+
+      {selectedCountry ? (
+        <div>
+          <h2>{selectedCountry.name.common}</h2>
+          <p>Capital: {selectedCountry.capital}</p>
+          <p>Population: {selectedCountry.population}</p>
+          <p>Languages: {Object.values(selectedCountry.languages).join(', ')}</p>
+          <img src={selectedCountry.flags.png} alt={`Flag of ${selectedCountry.name.common}`} width="150" />
+          <button onClick={() => setSelectedCountry(null)}>Back</button>
+        </div>
+      ) : (
+        filteredCountries.length > 10 ? (
+          <p>Too many matches, please specify another filter</p>
+        ) : filteredCountries.length === 1 ? (
+          <div>
+            <h2>{filteredCountries[0].name.common}</h2>
+            <p>Capital: {filteredCountries[0].capital}</p>
+            <p>Population: {filteredCountries[0].population}</p>
+            <p>Languages: {Object.values(filteredCountries[0].languages).join(', ')}</p>
+            <img src={filteredCountries[0].flags.png} alt={`Flag of ${filteredCountries[0].name.common}`} width="150" />
+          </div>
+        ) : (
+          <ul>
+            {filteredCountries.map(country => (
+              <li key={country.name.common}>
+                {country.name.common}
+                <button onClick={() => handleShowCountry(country)}>Show</button>
+              </li>
+            ))}
+          </ul>
+        )
+      )}
     </div>
   );
 };
