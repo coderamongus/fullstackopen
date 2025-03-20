c69fe3695ec94a44995a6f0a458db2fed82b7caa
coderamongus <simo.ahlstrand@gmail.com>
2025-03-20 14:15:15 +0200

    5.14 ja 5.15

--- part5.1/src/components/Blog.test.jsx
+++ part5.1/src/components/Blog.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { describe, test, expect } from 'vitest'
 import Blog from './Blog'
 import '@testing-library/jest-dom'
@@ -23,13 +23,43 @@ const currentUser = {
 describe('<Blog />', () => {
   test('renders blog title and author by default, but not url or likes', () => {
     render(<Blog blog={blog} currentUser={currentUser} handleLike={() => {}} handleDelete={() => {}} />)
-
     expect(screen.getByText('Test Blog Title - Test Author')).toBeInTheDocument()
-
     const urlElement = screen.queryByText(/http:\/\/testurl\.com/i)
     expect(urlElement).not.toBeInTheDocument()
-
     const likesElement = screen.queryByText(/Likes:/i)
     expect(likesElement).not.toBeInTheDocument()
   })
+
+  test('shows url, likes, and user info when the "Show" button is clicked', () => {
+    render(<Blog blog={blog} currentUser={currentUser} handleLike={() => {}} handleDelete={() => {}} />)
+  
+    const viewButton = screen.getByText(/show/i)
+    fireEvent.click(viewButton)
+
+    expect(screen.getByText('http://testurl.com')).toBeInTheDocument()
+    expect(screen.getByText(/Likes: 5/i)).toBeInTheDocument()
+    expect(screen.getByText(/Username: testuser/i)).toBeInTheDocument()
+  })
+  
+  test('calls handleLike twice when like button is clicked twice', () => {
+    const mockHandleLike = vi.fn()
+  
+    render(
+      <Blog
+        blog={blog}
+        currentUser={currentUser}
+        handleLike={mockHandleLike}
+        handleDelete={() => {}}
+      />
+    )
+  
+    const showButton = screen.getByText(/show/i)
+    fireEvent.click(showButton)
+    const likeButton = screen.getByRole('button', { name: /like/i })
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+  
+    expect(mockHandleLike).toHaveBeenCalledTimes(2)
+  })
+  
 })
